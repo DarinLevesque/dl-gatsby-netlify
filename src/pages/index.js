d3bf73383bc9c005e00233f65dc59f44e4a4e004
Darin Levesque <darinlevesque@gmail.com>
2018-04-12 16:34:30 -0400

    removed TypeIt temporality

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,34 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Link from "gatsby-link";
-import Script from "react-load-script";
-const isBrowser = typeof window !== "undefined";
-const TypeIt = isBrowser ? require("typeit") : undefined;
-import graphql from "graphql";
 
 export default class IndexPage extends React.Component {
   render() {
     const { data } = this.props;
     const { edges: posts } = data.allMarkdownRemark;
-    const { typed } = new TypeIt("#typed", {
-      speed: 50,
-      autoStart: false
-    })
-      .type("Wll")
-      .pause(500)
-      .delete(2)
-      .type("ell, ")
-      .pause(1000)
-      .type("I guess I'm typing..")
-      .break()
-      .pause(750)
-      .type(" but I don't really know what to say")
-      .options({ speed: 700 })
-      .type("...")
-      .pause(750)
-      .options({ speed: 50 })
-      .delete()
-      .type("IS THAT SO <strong>WRONG??</strong>");
 
     return (
       <section className="section">
