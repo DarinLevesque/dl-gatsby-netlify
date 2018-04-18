de2d657b9ce5a1554c78cd891835e37ddd8cee16
Darin Levesque <darinlevesque@gmail.com>
2018-04-18 16:14:26 -0400

    conditionally loaded typeit for webpack 'window' error

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Link from "gatsby-link";
-import TypeIt from "typeit";
+const isBrowser = typeof window !== "undefined";
+const TypeIt = isBrowser ? require("typeit") : undefined;
 
 export default class IndexPage extends React.Component {
   render() {
     const instance = new TypeIt("#typed", {
       strings: ["This is a great string.", "But here is a better one."],
-      speed: 50,
+      speed: 40,
       breakLines: false,
       autoStart: false
     });
@@ -16,7 +17,7 @@ export default class IndexPage extends React.Component {
       <section className="section">
         <div className="container">
           <div className="content">
-            <h1 className="has-text-weight-bold is-size-2">Latest Stories</h1>
+            <h1 className="has-text-weight-bold is-size-2">Background</h1>
           </div>
           <p id="typed" />
         </div>
@@ -24,32 +25,3 @@ export default class IndexPage extends React.Component {
     );
   }
 }
-
-IndexPage.propTypes = {
-  data: PropTypes.shape({
-    allMarkdownRemark: PropTypes.shape({
-      edges: PropTypes.array
-    })
-  })
-};
-
-export const pageQuery = graphql`
-  query IndexQuery {
-    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
-      edges {
-        node {
-          excerpt(pruneLength: 400)
-          id
-          fields {
-            slug
-          }
-          frontmatter {
-            title
-            templateKey
-            date(formatString: "MMMM DD, YYYY")
-          }
-        }
-      }
-    }
-  }
-`;
