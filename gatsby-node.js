a51126521a9706a78c3040074c4a1dea503eaf90
Darin Levesque <darinlevesque@gmail.com>
2018-05-14 21:55:00 -0400

    moved tags

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,9 @@
 const _ = require("lodash");
 const path = require("path");
-const { createFilePath } = require("gatsby-source-filesystem");
+const {createFilePath} = require("gatsby-source-filesystem");
 
-exports.createPages = ({ boundActionCreators, graphql }) => {
-  const { createPage } = boundActionCreators;
+exports.createPages = ({boundActionCreators, graphql}) => {
+  const {createPage} = boundActionCreators;
 
   return graphql(`
     {
@@ -24,7 +24,9 @@ exports.createPages = ({ boundActionCreators, graphql }) => {
     }
   `).then(result => {
     if (result.errors) {
-      result.errors.forEach(e => console.error(e.toString()));
+      result
+        .errors
+        .forEach(e => console.error(e.toString()));
       return Promise.reject(result.errors);
     }
 
@@ -35,9 +37,7 @@ exports.createPages = ({ boundActionCreators, graphql }) => {
       createPage({
         path: edge.node.fields.slug,
         tags: edge.node.frontmatter.tags,
-        component: path.resolve(
-          `src/templates/${String(edge.node.frontmatter.templateKey)}.js`
-        ),
+        component: path.resolve(`src/templates/${String(edge.node.frontmatter.templateKey)}.js`),
         // additional data can be passed via context
         context: {
           id
@@ -71,15 +71,11 @@ exports.createPages = ({ boundActionCreators, graphql }) => {
   });
 };
 
-exports.onCreateNode = ({ node, boundActionCreators, getNode }) => {
-  const { createNodeField } = boundActionCreators;
+exports.onCreateNode = ({node, boundActionCreators, getNode}) => {
+  const {createNodeField} = boundActionCreators;
 
   if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode });
-    createNodeField({
-      name: `slug`,
-      node,
-      value
-    });
+    const value = createFilePath({node, getNode});
+    createNodeField({name: `slug`, node, value});
   }
 };
